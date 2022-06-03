6a9e9dbbe82708bf9128487daa840a39a3dbbb37
a2c <atsushi1230.sdi@gmail.com>
2022-06-03 23:28:08 +0900

    renew

--- pages/lib/constants.ts
+++
@@ -1,7 +0,0 @@
-export const toFahrenheit = (degreeCelsius: number) => {
-  return degreeCelsius * 1.8 + 32;
-};
-
-export const toCelsius = (degreeFahrenheit: number) => {
-  return (degreeFahrenheit - 32) / 1.8;
-};
